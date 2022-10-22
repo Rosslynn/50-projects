6403bd41454d399955c73fb3ea218c2a21df73e6
Lynross <mauriciobarva@gmail.com>
2022-10-22 15:37:50 -0500

    feat: .container class added

---
+++ base/index.js
@@ -0,0 +1,23 @@
+const images =  [
+  {
+    title: 'explore the world',
+    url: 'https://images.unsplash.com/photo-1558979158-65a1eaa08691?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+    defaultActive: true,
+  },
+  {
+    title: 'wild forest',
+    url: 'https://images.unsplash.com/photo-1572276596237-5db2c3e16c5d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+  },
+  {
+    title: 'sunny beach',
+    url: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1353&q=80',
+  },
+  {
+    title: 'city on water',
+    url: 'https://images.unsplash.com/photo-1551009175-8a68da93d5f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80',
+  },
+  {
+    title: 'mountains - clouds',
+    url: 'https://images.unsplash.com/photo-1549880338-65ddcdfd017b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+  },
+];
